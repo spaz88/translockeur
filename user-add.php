2386799f5ae41310d4ab836485adfd4e5d685303
Anna <65238467+Anndec44@users.noreply.github.com>
2021-10-14 16:22:21 +0200

    Update user-add.php

--- user-add.php
+++ user-add.php
@@ -1,17 +1,30 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+session_start();
+include './connexion.inc.php';
+
+$page = 'user-add';
+include './header.inc.php';
+
+?>
+
+<div class="" data-aos="fade">
+    <div class="container">
+
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <h2 class="site-section-heading text-center">Ajout d'utilisateurs</h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <p class="text-center">
+                    Pour ne plus laisser trainer vos artices !
+                </p>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<?php include 'footer.inc.php'; ?>
