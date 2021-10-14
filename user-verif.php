cfbf457d14f1f9aac5a19edf5d956e0e5db84287
spaz88 <69171126+spaz88@users.noreply.github.com>
2021-10-14 17:14:30 +0200

    Add files via upload

---
+++ user-verif.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+include './connexion.inc.php';
+
+$page = 'user-verif';
+include './header.inc.php';
+
+ if(($_GET['action'] == "user-add") && !empty($_POST)){
+                        
+                        $nom = $_POST['nom'];
+                        $prenom = $_POST['prenom'];
+                        $pseudo = $_POST['pseudo'];
+                        $password = $_POST['password'];
+                        
+                    $sql = ("INSERT INTO `user` (`nom`, `prenom`, `username`, `password`, `avatar`, `commentaire`, `role`) VALUES
+                            ('$nom', '$prenom', '$pseudo', '$password','','','');");
+                    
+                    $resultats = $connexion->exec($sql);
+                    
+                    
+                    
+
+?>
+
+<div class="" data-aos="fade">
+    <div class="container">
+
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <h2 class="site-section-heading text-center">Bravo !</h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <p class="text-center">
+                    Vous avez réussi a créer votre compte !
+                </p>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<?php
+ }
+ else{
+?>
+<div class="" data-aos="fade">
+    <div class="container">
+
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <h2 class="site-section-heading text-center">Oops</h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <p class="text-center">
+                    Une erreur est survenu !
+                </p>
+            </div>
+        </div>
+
+    </div>
+</div>
+<?php
+ }
+?>
+<?php include 'footer.inc.php'; ?>
