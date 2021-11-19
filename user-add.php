4e4517b1789067c891db1b16cbacd090c25a10f0
spaz88 <69171126+spaz88@users.noreply.github.com>
2021-11-19 08:25:01 +0100

    Create user-add.php

--- user-add.php
+++ user-add.php
@@ -25,35 +25,35 @@ include './header.inc.php';
 
 
                         <div class="form-group">
-                            <label class="col-md-4 controle-label" for="nom">Nom</label>
+                            <label class="col-md-4 controle-label" for="nom">Nom<span style='color:red;'>*</span></label>
                             <div class="col-md-4">
                                 <input id="user-nom" name="nom" class="form-control input-md" type="text">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-4 controle-label" for="prenom">Prenom</label>
+                            <label class="col-md-4 controle-label" for="prenom">Prenom<span style='color:red;'>*</span></label></label>
                             <div class="col-md-4">
                                 <input id="user-prenom" name="prenom" class="form-control input-md" type="text">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-4 controle-label" for="pseudo">Pseudo</label>
+                            <label class="col-md-4 controle-label" for="pseudo">Pseudo<span style='color:red;'>*</span></label></label>
                             <div class="col-md-4">
                                 <input id="user-pseudo" name="pseudo" class="form-control input-md" type="text">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-4 controle-label" for="password">Mot de passe</label>
+                            <label class="col-md-4 controle-label" for="password">Mot de passe<span style='color:red;'>*</span></label></label>
                             <div class="col-md-4">
                                 <input id="user-password" name="password" class="form-control input-md" type="password">
                             </div>
                         </div>    
 
                         <div class="form-group">
-                            <label class="col-md-4 controle-label" for="avatar">Avatar</label>
+                            <label class="col-md-4 controle-label" for="avatar">Avatar<span style='color:red;'>*</span></label></label>
                             <div class="col-md-4">
                                 <input id="user-avatar" name="avatar" class="input-file" type="file">
                             </div>
